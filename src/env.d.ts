c5a78d486b9536038edf15f101d277b07781711b
incognitotgt <mashoor539@icloud.com>
2024-02-23 22:17:56 -0500

    im high on something anyways im making a new branch and rewriting all the js

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+interface Window {
+  navigator: {
+  keyboard: {
+    lock: (keys: string[]) => void;
+  };
+};
+  __uv$config: {
+    encodeUrl: (url: string) => string;
+  };
+}
